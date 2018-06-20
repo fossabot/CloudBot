4992fa5fa9e26825528b50cbeb7481e6930c6681
Alexander <vuur@users.noreply.github.com>
2018-06-19 21:25:24 -0400

    Revert

--- commands/wiki.js
+++ commands/wiki.js
@@ -1,10 +1,8 @@
-// Wiki.js start
-const embed = new Discord.RichEmbed()
-  .setColor(#cc33ff)
-  .setDescription("Require help? Check out our [documentation](https://github.com/Cloudbox/Cloudbox/wiki).")
-  .setThumbnail("https://i.imgur.com/jLqqJPP.png")
-  .addField("FAQ","[Frequently asked questions](https://github.com/Cloudbox/Cloudbox/wiki/FAQ).",
-  "Updating Cloudbox", "[Errors during git pull](https://github.com/Cloudbox/Cloudbox/wiki/FAQ#updating-cloudbox).",
-  "Error 502","[502](https://github.com/Cloudbox/Cloudbox/wiki/FAQ#error-502).")
-  // Send message
-  message.channel.send({embed});
+exports.run = (client, message, args) => {
+message.channel.send({embed: {
+  color: 3447003,
+  description: `
+Check out our Wiki!
+https://github.com/Cloudbox/Cloudbox/wiki`
+}});
+}
