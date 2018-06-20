bb6e88423f6fb97e4f747d64ed6e415176d1d51b
Alexander <vuur@users.noreply.github.com>
2018-06-19 21:20:42 -0400

    Test

--- commands/wiki.js
+++ commands/wiki.js
@@ -1,17 +1,10 @@
 // Wiki.js start
 const embed = new Discord.RichEmbed()
-  // Description
+  .setColor(#cc33ff)
   .setDescription("Require help? Check out our [documentation](https://github.com/Cloudbox/Cloudbox/wiki).")
-  // Select color purple
-  .setcolor(11141554)
-  // Thumbnail for github
   .setThumbnail("https://i.imgur.com/jLqqJPP.png")
-  // Extra options fields
-  // FAQ
-  .addField("FAQ","[Frequently asked questions](https://github.com/Cloudbox/Cloudbox/wiki/FAQ).")
-  // Updating cloudbox
-  .addfield("Updating Cloudbox", "[Errors during git pull](https://github.com/Cloudbox/Cloudbox/wiki/FAQ#updating-cloudbox).")
-  // Error 502
-  .addfield("Error 502","[502](https://github.com/Cloudbox/Cloudbox/wiki/FAQ#error-502).");
-// Send message
-message.channel.send({embed});
+  .addField("FAQ","[Frequently asked questions](https://github.com/Cloudbox/Cloudbox/wiki/FAQ).",
+  "Updating Cloudbox", "[Errors during git pull](https://github.com/Cloudbox/Cloudbox/wiki/FAQ#updating-cloudbox).",
+  "Error 502","[502](https://github.com/Cloudbox/Cloudbox/wiki/FAQ#error-502).")
+  // Send message
+  message.channel.send({embed});
